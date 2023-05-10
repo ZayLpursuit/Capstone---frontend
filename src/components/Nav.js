cd5513f9ffb3710d63ef121288b920699a634d5d
Tech-Kat <katpelle@pursuit.org>
2023-05-09 21:39:22 -0400

    changes

---
+++ src/components/Nav.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../Melanated-Diamonds.png";
+
+const Nav = () => {
+  return (
+    <div className="Nav">
+      <h1>
+        <Link to="/">
+          <img
+            src={Logo}
+            alt="Logo"
+            className="Logo"
+            width="200"
+            height="200"
+          />
+        </Link>
+      </h1>
+      <div className="nav-btns">
+        <button className="nav-btn">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </button>
+        {/* <button className="nav-btn">
+          <Link to="/survey" className="nav-link">
+            Survey
+          </Link>
+        </button>
+        <button className="nav-btn">
+          <Link to="/facts" className="nav-link">
+            Facts
+          </Link>
+        </button>
+        <button className="nav-btn">
+          <Link to="/resources" className="nav-link">
+            Resources
+          </Link>
+        </button> */}
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
