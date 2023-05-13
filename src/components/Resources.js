099f161a6280b088748f22bc85722c6a2981174f
Wwright91 <wisdomwright-martin@pursuit.org>
2023-05-13 17:10:52 -0400

    component and route created

---
+++ src/components/Resources.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Resources = () => {
+  return (
+    <div>
+      <section>
+        <h2>Grants</h2>
+      </section>
+
+      <section>
+        <h2>Scholarships</h2>
+      </section>
+    </div>
+  );
+};
+
+export default Resources;
