afaf35c72ac205dc7e5f34174bcbe683eb98716d
Kendra Pelle <107086930+Tech-Kat@users.noreply.github.com>
2023-05-15 23:33:48 -0400

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
       <h2 className="statement">
        is to support and promote black-owned businesses by creating a platform for consumers to connect with them, with the goal of increasing their visibility and helping them thrive. They aim to promote entrepreneurship and economic equity within the black community, ultimately building a better and more inclusive economy for all.
       </h2>
-    <br />
+     <br />
       <h1>Celebrate Us</h1>
     </div> 
   );
