fbd40d0fe46c89a2cd64bdc9847b721294ddf1bd
Tech-Kat <katpelle@pursuit.org>
2023-05-15 23:29:00 -0400

    fixed

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,14 +3,14 @@ import React from "react";
 const Home = () => {
   return (
     <div className="home">
-      {/* <h1>Our Mission</h1> */}
-      {/* <br /> */}
-      {/* <h2 className="statement">
+      <h1>Our Mission</h1> 
+      <br />
+      <h2 className="statement">
        is to support and promote black-owned businesses by creating a platform for consumers to connect with them, with the goal of increasing their visibility and helping them thrive. They aim to promote entrepreneurship and economic equity within the black community, ultimately building a better and more inclusive economy for all.
-      </h2> */}
-      {/* <br />
-      <h1>Celebrate Us</h1> */}
-    </div>
+      </h2>
+    <br />
+      <h1>Celebrate Us</h1>
+    </div> 
   );
 };
 
