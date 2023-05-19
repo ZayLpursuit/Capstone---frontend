34b5e6900005ef858d01e37767bfde0e516ad184
Tech-Kat <katpelle@pursuit.org>
2023-05-19 11:57:48 -0400

    scholarships

--- src/components/CulturalFacts.js
+++ src/components/CulturalFacts.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const data = require("../data/data.json")
+const data = require("../data/data.json");
 
 const CulturalFacts = () => {
-  let fact = data.map(f => f.fact)
+  let fact = data.map((f) => f.fact);
 
   // console.log(fact)
 
   return (
-    <div className='cultural-facts'>
-      <h6 style={{textDecoration: "underline"}}>Facts On Deck</h6>
-      {(<h5>{fact[Math.floor(Math.random() * fact.length)]}</h5>)}
+    <div id="scroll-container">
+      <div className="cultural-facts">
+        <h6 style={{ textDecoration: "underline" }}>Facts On Deck</h6>
+        <div id="scroll-text">
+          {<h5>{fact[Math.floor(Math.random() * fact.length)]}</h5>}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CulturalFacts
+export default CulturalFacts;
--- src/components/Resources.js
+++ src/components/Resources.js
@@ -4,84 +4,119 @@ import scholars from "../assets/fotor_2023-5-16_10_18_40.png";
 const Resources = () => {
   return (
     <div className="resources">
-      <img className="center-text scholars" src={scholars} width="60%" height="140px" alt="n/a"/>
+      <img
+        className="center-text scholars"
+        src={scholars}
+        width="60%"
+        height="140px"
+        alt="n/a"
+      />
       <div className="resource-section">
-      <section className="sidebar">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOLf5vKJpaJPP2AW_BVIDYwEZovw-0YAmOYA&usqp=CAU" className="resource-img" alt="n/a"/>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOLf5vKJpaJPP2AW_BVIDYwEZovw-0YAmOYA&usqp=CAU" className="resource-img" alt="n/a"/>
-      </section>
-      <section className="center-text">
-        {/* <h2 style={{ textDecoration: "underline" }}>Grants / Scholarships</h2> */}
-        {/* <img src={scholars} width="8000px" height="100px" /> */}
-        {/* <br /> */}
-        <br />
-        <div className="scholarships">
-        <ul>
-          <li>
-            <a href="https://uncf.org/scholarships">
-              United Negro College Fund (UNCF)
-            </a>
-          </li>
-          <li>
-            <a href="https://jackierobinson.org/scholarship/">
-              Jackie Robinson Foundation (JFR)
-            </a>
-          </li>
-          <li>
-            <a href="http://www.tmcf.org/students-alumni/scholarships/">
-              Thurgood Marshall College Fund (TMCF)
-            </a>
-          </li>
-          <li>
-            <a href="https://ronbrown.org/apply/ron-brown-scholarship/">
-              Ron Brown Scholarship
-            </a>
-          </li>
-          <li>
-            <a href="https://www.blmsquaredscholarship.org/applicants">
-              Black Lives Matter Squared Scholarship Foundation (BLM²)
-            </a>
-          </li>
-          <li>
-            <a href="https://www.ametsoc.org/index.cfm/ams/information-for/students/ams-scholarships-and-fellowships/ams-scholarship-for-underserved-communities-formerly-minority-scholarship/">
-              AMS Scholarship for Underserved Communities
-            </a>
-          </li>
-          <li>
-            <a href="https://nanbpwc.org/wp-content/uploads/2023/01/2023%20Malveaux%20Scholarship%20App.pdf">
-              Dr. Julianne Malveaux Scholarship
-            </a>
-          </li>
-          <li>
-            <a href="https://www.pearlmillingcompany.com/pearlpledge/applynow">
-              P.E.A.R.L. Pledge
-            </a>
-          </li>
-          <li>
-            <a href="https://lpwhitehead.org/grants-program/">
-              Lettie Pate Whitehead Foundation
-            </a>
-          </li>
-          <li>
-            <a href="https://financialservices.howard.edu/resources/hu-need-based-grant-needy">
-              Howard University Need Bases Grant
-            </a>
-          </li>
-          <li>
-            <a href="https://www.bcala.org/2022-2023-e-j-josey-scholarship?gad=1&gclid=CjwKCAjwjYKjBhB5EiwAiFdSfnHOihJRm8UozJNQeRW7tXZO2dgdX1IsWqtQGAiz5bIrK9bOgwH6NhoC2DEQAvD_BwE">
-              Black Caucus of the American Library Association
-            </a>
-          </li>
-          {/* <li></li>
-          <li></li> */}
-          </ul>
-          </div>
-      </section>
-      <section className="sidebar">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOLf5vKJpaJPP2AW_BVIDYwEZovw-0YAmOYA&usqp=CAU" className="resource-img" alt="n/a"/>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOLf5vKJpaJPP2AW_BVIDYwEZovw-0YAmOYA&usqp=CAU" className="resource-img" alt="n/a"/>
+        <section className="sidebar">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOLf5vKJpaJPP2AW_BVIDYwEZovw-0YAmOYA&usqp=CAU"
+            className="resource-img"
+            alt="n/a"
+          />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOLf5vKJpaJPP2AW_BVIDYwEZovw-0YAmOYA&usqp=CAU"
+            className="resource-img"
+            alt="n/a"
+          />
         </section>
-        </div>
+        <section className="center-text">
+          {/* <h2 style={{ textDecoration: "underline" }}>Grants / Scholarships</h2> */}
+          {/* <img src={scholars} width="8000px" height="100px" /> */}
+          {/* <br /> */}
+          <br />
+          <div className="scholarships">
+            <ul>
+              <li>
+                <a href="https://uncf.org/scholarships">
+                  United Negro College Fund (UNCF)
+                </a>
+              </li>
+              <li>
+                <a href="https://jackierobinson.org/scholarship/">
+                  Jackie Robinson Foundation (JFR)
+                </a>
+              </li>
+              <li>
+                <a href="http://www.tmcf.org/students-alumni/scholarships/">
+                  Thurgood Marshall College Fund (TMCF)
+                </a>
+              </li>
+              <li>
+                <a href="https://ronbrown.org/apply/ron-brown-scholarship/">
+                  Ron Brown Scholarship
+                </a>
+              </li>
+              <li>
+                <a href="https://www.blmsquaredscholarship.org/applicants">
+                  Black Lives Matter Squared Scholarship Foundation (BLM²)
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ametsoc.org/index.cfm/ams/information-for/students/ams-scholarships-and-fellowships/ams-scholarship-for-underserved-communities-formerly-minority-scholarship/">
+                  AMS Scholarship for Underserved Communities
+                </a>
+              </li>
+              <li>
+                <a href="https://nanbpwc.org/wp-content/uploads/2023/01/2023%20Malveaux%20Scholarship%20App.pdf">
+                  Dr. Julianne Malveaux Scholarship
+                </a>
+              </li>
+              <li>
+                <a href="https://www.pearlmillingcompany.com/pearlpledge/applynow">
+                  P.E.A.R.L. Pledge
+                </a>
+              </li>
+              <li>
+                <a href="https://lpwhitehead.org/grants-program/">
+                  Lettie Pate Whitehead Foundation
+                </a>
+              </li>
+              <li>
+                <a href="https://financialservices.howard.edu/resources/hu-need-based-grant-needy">
+                  Howard University Need Bases Grant
+                </a>
+              </li>
+              <li>
+                <a href="https://www.bcala.org/2022-2023-e-j-josey-scholarship?gad=1&gclid=CjwKCAjwjYKjBhB5EiwAiFdSfnHOihJRm8UozJNQeRW7tXZO2dgdX1IsWqtQGAiz5bIrK9bOgwH6NhoC2DEQAvD_BwE">
+                  Black Caucus of the American Library Association
+                </a>
+              </li>
+              <li>
+                <a href="https://www.floridajobs.org/community-planning-and-development/Small-and-Minority-Business-Resources/Small-and-Minority-Business-Programs/black-business-loan-program">
+                  Black Business Loan Program (BBLP)
+                </a>
+              </li>
+              <li>
+                <a href="https://www.nsbe.org/collegiate/education/scholarships">
+                  National Society of Black Engineers Scholarships
+                </a>
+              </li>
+              <li>
+                <a href="https://naacp.org/find-resources/scholarships-awards-internships">
+                  NAACP Scholarships
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section className="sidebar">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOLf5vKJpaJPP2AW_BVIDYwEZovw-0YAmOYA&usqp=CAU"
+            className="resource-img"
+            alt="n/a"
+          />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOLf5vKJpaJPP2AW_BVIDYwEZovw-0YAmOYA&usqp=CAU"
+            className="resource-img"
+            alt="n/a"
+          />
+        </section>
+      </div>
     </div>
   );
 };
