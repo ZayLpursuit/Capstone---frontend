09389e54dc7430df37100706b999208f11a8f0af
ZayLpursuit <Zaylors@pursuit.org>
2023-05-23 21:35:37 -0400

    map added to indexPage  and can create markers.Needs styling

---
+++ src/components/MapContainer.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import { Map, GoogleApiWrapper,Marker } from 'google-maps-react';
+
+
+const MapContainer = ({businesses}) => {
+
+  const styles={
+    map:{
+      height:"100%",
+      "width":"50%",
+      "grid-column":"2"
+    }
+  }
+
+
+  const [markers,setMarkers]=useState([])
+
+  const geocoder= new window.google.maps.Geocoder()
+
+  useEffect(()=>{
+    businesses.forEach(({address})=>{
+      geocoder.geocode({"address":address},(results,status)=>{
+        if(status=='OK'){
+          const {lat,lng}=results[0].geometry.location 
+         
+
+          // setMarkers([...markers,{"address":address,"position":{"lat":lat,"lng":lng}}])  
+          setMarkers((prev)=>[...prev,{address,position:{lat:lat(),lng:lng()}}])
+          
+      }
+      else{
+        console.error(`Geocoding error:${status}`)
+      }
+      })
+    })
+  },[businesses])
+
+  return (
+    <div className="map-container">
+    <Map
+      google={window.google}
+      zoom={12}
+      initialCenter={{ lat: 40.7128, lng: -74.0060 }} // Set the initial map center
+      style={styles.map}
+      >
+
+       {markers.map((marker,idx)=>{
+       console.log(marker)
+        
+      return  (
+        <Marker
+        key={idx}
+        position={marker.position}
+        label={marker.address}
+        />)
+      })}
+      </Map>
+     
+
+
+   
+   </div>
+  );
+};
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDRaYXfJoUnKseZY0JfTroxjhUWCDf64Lc',
+})(MapContainer);
