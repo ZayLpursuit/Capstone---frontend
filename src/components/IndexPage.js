956c420777d8e91225be80bf69f69d2c2e88b8e0
Wwright91 <wisdomwright-martin@pursuit.org>
2023-06-13 22:34:54 -0400

    pictures added to the category buttons

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -1,103 +1,99 @@
-// import { IconButton } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Card from "./Card";
 import MapContainer from "./MapContainer";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-
-// import resIcon from "../assets/knife_fork.png"
 import { Button, ButtonGroup } from "@mui/material";
 
 const API = process.env.REACT_APP_API_URL;
 
 export default function IndexPage() {
-  const [category, setCategory] = useState("");
+  const [category, setCategory] = useState(null);
   const [display, setDisplay] = useState([]);
   function handleChange(e) {
     setCategory(e.target.id);
     // axios.get(`http://localhost:7777/businesses/categories/${category}`).then((res)=>console.log(res))
   }
   useEffect(() => {
-    axios
-      .get(`${API}/businesses/categories/${category}`)
-      .then((res) => setDisplay(res.data));
+    const url = category
+      ? `http://localhost:7777/businesses/categories/${category}`
+      : `http://localhost:7777/businesses`;
+    axios.get(url).then((res) => setDisplay(res.data));
   }, [category]);
-
-  // useEffect(() => {
-  //   axios
-  //     .get(`http://localhost:7777/businesses`)
-  //     .then((res) => setDisplay(res.data));
-  // }, []);
 
   return (
     <div className="index-grid">
       <div className="grid test-b1">
         <div className="m-left">
-          <aside className=" ">
+          <aside className="category-section">
             <h1 className="select-cat">Categories:</h1>
-
-            <ButtonGroup>
-              <Button id="restaurant" onClick={(e) => handleChange(e)}>Restaurants
-                {" "}{" "}
-                <br/>
-                <i class="fa-solid fa-utensils"></i></Button>
-</ButtonGroup>
-
-
-
-
-            {/* <ul className="category-options"> */}
-              {/* <ButtonGroup variant="outlined" aria-label="outlined button group"> */}
-                {/* <Button> */}
-                {/* <FontAwesomeIcon icon="fa-sharp fa-solid fa-fork-knife" style={{color: "#000000"}} /> */}
-                {/* </Button> */}
-              {/* </ButtonGroup> */}
-              {/* <li id="restaurant" onClick={(e) => handleChange(e)}> */}
-                {/* <FontAwesomeIcon icon="fa-solid fa-fork-knife" /> */}
-                {/* <FontAwesomeIcon icon="fa-solid fa-fork-knife" style={{color: "#000000",}} /> */}
-                {/* <img src={resIcon}
-                /> */}
-                {/* Restaraunts <i class="fa-solid fa-utensils"></i>
-              </li>
-              <li id="social" onClick={(e) => handleChange(e)}>
-                Social
-              </li>
-              <li id="barber shop" onClick={(e) => handleChange(e)}>
-                Barber Shops
-              </li>
-              <li id="bank" onClick={(e) => handleChange(e)}>
-                Banks
-              </li>
-              <li id="fashion" onClick={(e) => handleChange(e)}>
-                Fashion
-              </li>
-              <li id="beauty" onClick={(e) => handleChange(e)}>
-                Beauty
-              </li>
-              <li id="health and wellness" onClick={(e) => handleChange(e)}>
-                Health and Wellness
-              </li>
-            </ul> */}
+            <br />
+            <ButtonGroup orientation="vertical" className="buttons-group">
+              <Button
+                className="categories"
+                id="restaurant"
+                onClick={(e) => handleChange(e)}
+              >
+                <h4>Restaurants</h4>
+                <p>&#x1F374;</p>
+              </Button>
+              <Button
+                className="categories"
+                id="social"
+                onClick={(e) => handleChange(e)}
+              >
+                <h4>Social</h4>
+                <p>&#x1F3B6;</p>
+              </Button>
+              <Button
+                className="barber"
+                id="barbershop"
+                onClick={(e) => handleChange(e)}
+              >
+                <h4>Barber Shops</h4>
+                <p>&#x1F488;</p>
+              </Button>
+              <Button
+                className="categories"
+                id="bank"
+                onClick={(e) => handleChange(e)}
+              >
+                <h4>Banks</h4>
+                <p>&#x1F4B0;</p>
+              </Button>
+              <Button
+                className="categories"
+                id="fashion"
+                onClick={(e) => handleChange(e)}
+              >
+                <h4>Fashion</h4>
+                <p>&#x1F460;</p>
+              </Button>
+              <Button
+                className="categories"
+                id="beauty"
+                onClick={(e) => handleChange(e)}
+              >
+                <h4>Beauty</h4>
+                <p>&#x1F484;</p>
+              </Button>
+              <Button
+                className="categories"
+                id="health-and-wellness"
+                onClick={(e) => handleChange(e)}
+              >
+                <h4>Health and Wellness</h4>
+                <p>&#x1F486;</p>
+              </Button>
+            </ButtonGroup>
           </aside>
-          {/* <div className="grid m-left">
-            <label htmlFor="online">
-              Online Only
-              <input type="checkbox" id="online" className="margin-top" />
-            </label>
-
-            <label htmlFor="in-store">
-              Has Store
-              <input type="checkbox" id="in-store" className="margin-top" />
-            </label>
-          </div> */}
         </div>
       </div>
 
-      {/* <div className="index-container test-b2">
+      <div className="index-container test-b2">
         {display?.map((business, idx) => {
           return <Card business={business} key={idx} />;
         })}
-      </div> */}
+      </div>
       <div className="col-3 index-map">
         <MapContainer businesses={display} />
       </div>
